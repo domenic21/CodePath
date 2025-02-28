a6a00fbc2d29a3b8484298f32772ef1f4830d30b
domenic21 <domenicarias21@gmail.com>
2025-02-28 02:25:39 -0500

    week 2

--- demo.py
+++ demo.py
@@ -57,15 +57,84 @@
 # sequence = [1, 6, -1, 10]
 # print(is_subsequence(lst, sequence)) 
 
-def is_subsequence(lst, sequence):
-    seq_index = 0
-    for i in range(len(lst)):
-        if seq_index == len(sequence): #if the index is equal to the length of the sequence
-            return True
-        if lst[i] == sequence[seq_index]: #if the value at the index of the list is equal to the value at the index of the sequence
-            seq_index += 1 #increment the index of the sequence
-    return seq_index == len(sequence) #return the index of the sequence is equal to the length of the sequence
+# def is_subsequence(lst, sequence):
+#     seq_index = 0
+#     for i in range(len(lst)):
+#         if seq_index == len(sequence): #if the index is equal to the length of the sequence
+#             return True
+#         if lst[i] == sequence[seq_index]: #if the value at the index of the list is equal to the value at the index of the sequence
+#             seq_index += 1 #increment the index of the sequence
+#     return seq_index == len(sequence) #return the index of the sequence is equal to the length of the sequence
 
-lst = [5, 1, 22, 25, 6, -1, 8, 10]
-sequence = [1, 6, -1, 10]
-print(is_subsequence(lst, sequence))  
+# lst = [5, 1, 22, 25, 6, -1, 8, 10]
+# sequence = [1, 6, -1, 10]
+# print(is_subsequence(lst, sequence))  
+
+# Problem 2: Create a Dictionary
+# create a dictionary from a list of keys and a list of values
+# create a for loop  for i in range i = 0 and the len of keys for each value, for each key we would assing value
+#
+# def create_dictionary(keys, values):
+#      dictionary ={}
+#      for i in range(0, len(keys)):
+#         dictionary[keys[i] ]= values[i]
+#         print(dictionary)
+# keys = ["peanut", "dragon", "star", "pop", "space"]
+# values = ["butter", "fly", "fish", "corn", "ship"]
+# create_dictionary(keys, values)
+     
+     #Session 2 Unit 2
+     #Define function cast_vote, the function accepts a dictionary 
+     #maps throught the candidates and assignes number of votes and a string + candidate
+     #If candidate doesnt exist add them to the dictionary 
+     # Find candidate 
+     #increase number +1 of votes 
+     #if not candidate not in the dictionary, create an entry and update dictionary
+     
+# def cast_vote(votes, candidate):
+    
+#     if candidate in votes:
+#         votes[candidate] += 1 # it would add 1 if candidate exists
+#     else:
+#         votes[candidate] = 1  #it would add a new candidate with 1 vote
+    
+# votes = {"Alice": 5, "Bob": 3}
+# cast_vote(votes, "Alice")
+# print(votes)
+# cast_vote(votes, "Gina")
+# print(votes)
+
+#Problem 2: Keys in Common 
+#Function takes 2 dictionaries and compares each other
+#Returns common keys 
+
+# def common_keys(dict1, dict2):
+#     lst = []
+#     for name in dict1.keys():
+#         for name2 in dict2.keys():
+#             if name == name2:
+#                 lst.append(name)
+#     return lst
+        
+    
+# dict1 = {"a": 1, "b": 2, "c": 3}
+# dict2 = {"b": 4, "c": 5, "d": 6}
+# common_list = common_keys(dict1, dict2)
+# print(common_list)
+
+#Problem 3: Highest Priority Task
+#Given a dictionary check which task has the highest priority and return
+# if priority has the same number 1-10  return the first in the alphabet
+
+def get_highest_priority_task(tasks):
+    highest_priority = -1
+    highest_priority_task = ""
+    for task, priority in tasks.items():
+        if priority > highest_priority or (priority == highest_priority and task < highest_priority_task):
+            highest_priority = priority
+            highest_priority_task = task
+    return highest_priority_task
+
+tasks = {"task1": 8, "task2": 1, "task3": 9, "task4": 10, "task5": 7}
+perform_task = get_highest_priority_task(tasks)
+print(perform_task)
